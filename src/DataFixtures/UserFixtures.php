50aac945e7f14fd769e2089a1d903b6e3babe349
Cid-Jie <pcidjie@gmail.com>
2022-06-19 14:36:47 +0200

    add new user role user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
@@ -20,14 +21,30 @@ class UserFixtures extends Fixture
     {
         $user = new User();
         $user
-            ->setEmail('thomas.aldaitz@wildcodeschool.com')
+            ->setEmail('pcidjie@monsite.com')
             ->setRoles(['ROLE_ADMIN'])
         ;
 
-        $hash = $this->passwordHasher->hashPassword($user, 'toto123');
+        $hash = $this->passwordHasher->hashPassword($user, 'cidjie');
         $user->setPassword($hash);
 
         $manager->persist($user);
         $manager->flush();
+
+        $faker = Factory::create();
+        $user2 = new User();
+        $user2
+            ->setEmail($faker->email())
+            ->setRoles(['ROLE_USER'])
+        ;
+        $hash2 = $this->passwordHasher->hashPassword(
+            $user2,
+            'userpass'
+        );
+        $user2->setPassword($hash2);
+
+        $manager->persist($user2);
+        $manager->flush();
+        
     }
 }
